724c6b594aabee78b7de73fe4d0ae6203d30bfef
SauravRana <sauravrana1983@hotmail.com>
2019-03-18 16:57:51 +0530

    Singleton in JS

--- desing-patterns/creational/singleton/singleton.js
+++ desing-patterns/creational/singleton/singleton.js
@@ -1,11 +1,34 @@
 class Person {
-    constructor(){
+    constructor(name=''){
         if(typeof Person.instance === 'object'){
             return Person.instance;
         }
         Person.instance = this;
+        this.name = name;
         return this;
+    }
+
+    getName(){
+        return this.name;
     }
 }
 
-export default Person;
+class Student extends Person {
+    getStudentName(){
+        return this.getName();
+    }
+}
+
+
+const firstInstance = new Person('First');
+const secondInstance = new Person('Second');
+const thirdInstance = new Person('');
+
+console.log(firstInstance.getName());
+console.log(secondInstance.getName());
+console.log(thirdInstance.getName());
+
+const student = new Student();
+
+console.log('getName' in student);  // returns true
+console.log('getStudentName' in student); // returns false  ---> Why this returns false?
