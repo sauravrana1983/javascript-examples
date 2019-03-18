6142f8af103c7e1f6b0039344c6e8d2473fec603
SauravRana <sauravrana1983@hotmail.com>
2019-03-18 17:00:10 +0530

    Removing test file

--- desing-patterns/creational/singleton/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-class Person {
-    constructor(){
-        if(typeof Person.instance === 'object'){
-            return Person.instance;
-        }
-        Person.instance = this;
-        return this;
-    }
-}
