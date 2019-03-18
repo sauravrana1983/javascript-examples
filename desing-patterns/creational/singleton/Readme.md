efb565e2379f5a89733fedd4a6c511e536b0da45
SauravRana <sauravrana1983@hotmail.com>
2019-03-18 15:00:10 +0530

    Intial Commit

---
+++ desing-patterns/creational/singleton/Readme.md
@@ -0,0 +1,3 @@
+# Singleton Design Pattern
+## Definition
+- Ensure a class has only one instance and provide a global point of access to it.
---
+++ desing-patterns/creational/singleton/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+class Person {
+    constructor(){
+        if(typeof Person.instance === 'object'){
+            return Person.instance;
+        }
+        Person.instance = this;
+        return this;
+    }
+}
